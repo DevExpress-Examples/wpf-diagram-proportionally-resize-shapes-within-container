0dbfc704501410eaaf1a79c4ad96cbd7cc5c39f2
DevExpressExampleBot <fakeEmail>
2023-08-17 15:50:17 +0400

    Source auto update [skip ci]

--- VB/WpfApp13/MainWindow.xaml.vb
+++ VB/WpfApp13/MainWindow.xaml.vb
@@ -20,11 +20,12 @@ Namespace WpfApp13
             Me.diagramControl1.Items.Add(CreateContainerShape1())
             AddHandler Me.diagramControl1.BeforeItemsResizing, AddressOf Me.DiagramControl1_BeforeItemsResizing
             AddHandler Me.diagramControl1.ItemsResizing, AddressOf Me.DiagramControl1_ItemsResizing
+            DiagramControl.ItemTypeRegistrator.Register(GetType(CustomDiagramContainer))
             AddHandler Loaded, AddressOf Me.MainWindow_Loaded
         End Sub
 
         Private Sub DiagramControl1_BeforeItemsResizing(ByVal sender As Object, ByVal e As DiagramBeforeItemsResizingEventArgs)
-            Dim containers = e.Items.OfType(Of DiagramContainer)()
+            Dim containers = e.Items.OfType(Of CustomDiagramContainer)()
             For Each container In containers
                 e.Items.Remove(container)
                 For Each item In container.Items
@@ -35,9 +36,9 @@ Namespace WpfApp13
 
         Private Sub DiagramControl1_ItemsResizing(ByVal sender As Object, ByVal e As DiagramItemsResizingEventArgs)
             Dim groups = e.Items.GroupBy(Function(x) x.Item.ParentItem)
-            Dim container As DiagramContainer = Nothing
+            Dim container As CustomDiagramContainer = Nothing
             For Each group In groups
-                If CSharpImpl.__Assign(container, TryCast(group.Key, DiagramContainer)) IsNot Nothing Then
+                If CSharpImpl.__Assign(container, TryCast(group.Key, CustomDiagramContainer)) IsNot Nothing Then
                     Dim containingRect = container.Items.[Select](Function(x) x.RotatedDiagramBounds().BoundedRect()).Aggregate(Rect.Empty, New Func(Of Rect, Rect, Rect)(AddressOf Rect.Union))
                     container.Position = New Point(containingRect.X, containingRect.Y)
                     container.Width = CSng(containingRect.Width)
@@ -50,8 +51,8 @@ Namespace WpfApp13
             Me.diagramControl1.FitToItems(Me.diagramControl1.Items)
         End Sub
 
-        Public Function CreateContainerShape1() As DiagramContainer
-            Dim container = New DiagramContainer() With {.Width = 200, .Height = 200, .Position = New Point(100, 100), .CanAddItems = False, .ItemsCanChangeParent = False, .ItemsCanCopyWithoutParent = False, .ItemsCanDeleteWithoutParent = False, .ItemsCanAttachConnectorBeginPoint = False, .ItemsCanAttachConnectorEndPoint = False}
+        Public Function CreateContainerShape1() As CustomDiagramContainer
+            Dim container = New CustomDiagramContainer() With {.Width = 200, .Height = 200, .Position = New Point(100, 100), .CanAddItems = False, .ItemsCanChangeParent = False, .ItemsCanCopyWithoutParent = False, .ItemsCanDeleteWithoutParent = False, .ItemsCanAttachConnectorBeginPoint = False, .ItemsCanAttachConnectorEndPoint = False}
             container.StrokeThickness = 0
             container.Background = Brushes.Transparent
             Dim innerShape1 = New DiagramShape() With {.CanSelect = True, .CanChangeParent = False, .CanEdit = True, .CanResize = False, .CanCopyWithoutParent = False, .CanDeleteWithoutParent = False, .CanMove = False, .Shape = BasicShapes.Trapezoid, .Height = 50, .Width = 200, .Content = "Custom text"}
@@ -69,5 +70,10 @@ Namespace WpfApp13
                 Return value
             End Function
         End Class
+    End Class
+
+    Public Class CustomDiagramContainer
+        Inherits DiagramContainer
+
     End Class
 End Namespace
